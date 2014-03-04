a32a015e8e6548362222a2b589ceaba7d7bffb05
Kritin Singh <2107613s@bo720-9-06.(none)>
2014-03-04 12:51:01 +0000

    Added User profile page(just user's detail)

--- labRatsApp/urls.py
+++ labRatsApp/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
 	url(r'^login/$', views.user_login, name = 'login'),
 	url(r'^logout/$', views.user_logout, name='logout'),
 	url(r'^restricted/$', views.restricted, name='restricted'),
-	url(r'^about/$', views.about, name='about')
+	url(r'^about/$', views.about, name='about'),
+	url(r'^profile/(?P<name>\w+)/$', views.profile, name='profile')
     
 )
